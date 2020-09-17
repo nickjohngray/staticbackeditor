3c4dbf74f24ea179fdbb87062abc1d5ac4aef825
nickjohngray <nickjohngray@gmail.com>
2020-09-17 22:23:47 +1200

    added forever

---
+++ src/scripts/start_prod.js
@@ -0,0 +1,32 @@
+
+const childProcess =  require('child_process')
+
+
+    try {
+        console.log('Starting Prod forever')
+
+        const p = childProcess.exec('npm run start-prod')
+
+        p.on('exit', (code) => {
+            let err = code === 0 ? null : new Error('exit code ' + code)
+            if (err) {
+                console.log(
+                    'an error occurred while trying to npm run start-dev error is ' + err
+                )
+                return
+            }
+
+            p.stdout.on('data', (message) => {
+                console.log('server>>> ' + message)
+            })
+
+            p.on('error', (code2) => {
+                console.log('server error Error is ' + code2)
+            })
+        })
+
+
+    } catch (err) {
+        console.error(err)
+    }
+
